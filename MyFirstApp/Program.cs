fac6e1cff6ee21e7d20f0cecdea5c96d172e7dfc
kannanATexcelforte <kannan@excelforte.com>
2024-01-31 11:47:04 +0530

    31.01.2024-hrh-completed

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,16 +3,11 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    if (1 == 1)
-    {
-        context.Response.StatusCode = 200;
-    }
-    else
-    {
-        context.Response.StatusCode = 400;
-    }
-    await context.Response.WriteAsync("Hello");
-    await context.Response.WriteAsync(" World");
+    context.Response.Headers["MyKey"] = "my value";
+    context.Response.Headers["Server"] = "My Server";
+    context.Response.Headers["Content-Type"] = "text/html";
+    await context.Response.WriteAsync("<h1>Hello</h1>");
+    await context.Response.WriteAsync(" <h2>World</h2>");
 });
 
 app.Run();
