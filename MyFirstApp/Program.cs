31532b8c7d0a87ca8cd8187aec866b67b8ddef77
kannanATexcelforte <kannan@excelforte.com>
2024-02-09 14:53:58 +0530

    09.02.2024-hr-completed

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,11 +3,12 @@ var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    context.Response.Headers["MyKey"] = "my value";
-    context.Response.Headers["Server"] = "My Server";
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+
     context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync("<h1>Hello</h1>");
-    await context.Response.WriteAsync(" <h2>World</h2>");
+    await context.Response.WriteAsync($"<p>{path}</p>");
+    await context.Response.WriteAsync($"<p>{method}</p>");
 });
 
 app.Run();
