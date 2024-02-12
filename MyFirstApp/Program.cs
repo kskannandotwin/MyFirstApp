756d128d0095ee7d755851fbcf7d1c272ecfcc57
kannanATexcelforte <kannan@excelforte.com>
2024-02-12 09:37:37 +0530

    12.02.2024-qs-completed

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,14 +1,19 @@
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.Run(async (HttpContext context) =>
 {
-    string path = context.Request.Path;
-    string method = context.Request.Method;
-
     context.Response.Headers["Content-Type"] = "text/html";
-    await context.Response.WriteAsync($"<p>{path}</p>");
-    await context.Response.WriteAsync($"<p>{method}</p>");
+    if (context.Request.Method == "GET")
+    {
+        if (context.Request.Query.ContainsKey("id"))
+        {
+            string id = context.Request.Query["id"];
+            await context.Response.WriteAsync($"<p>{id}</p>");
+        }
+    }
 });
 
 app.Run();
