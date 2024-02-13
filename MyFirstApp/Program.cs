302f9ef3d86152b25aa1f53f01429003ab0970fb
kannanATexcelforte <kannan@excelforte.com>
2024-02-13 10:40:38 +0530

    13.02.2024-hrh-completed

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -6,13 +6,10 @@ var app = builder.Build();
 app.Run(async (HttpContext context) =>
 {
     context.Response.Headers["Content-Type"] = "text/html";
-    if (context.Request.Method == "GET")
+    if (context.Request.Headers.ContainsKey("User-Agent"))
     {
-        if (context.Request.Query.ContainsKey("id"))
-        {
-            string id = context.Request.Query["id"];
-            await context.Response.WriteAsync($"<p>{id}</p>");
-        }
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<p>{userAgent}</p>");
     }
 });
 
